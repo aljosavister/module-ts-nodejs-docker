e1d5cd8680e681904e4e72a7375bf0d0253a732e
Aljoša Vister <aljosa.vister@acroni.si>
2023-09-16 19:23:46 +0200

    Semi working version

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 # Copy hosts file to etc
-cp /image-root/hosts /etc/
+cp /tmp/hosts /etc/
 
 # starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
